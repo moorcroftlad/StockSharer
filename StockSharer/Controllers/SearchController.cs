bc6e38fd1b8fb4e9ab928c321b9c3aa010bd27ed
Andrew Moorcroft <moorcroftlad@hotmail.com>
2015-11-30 10:03:28 +0000

    added mysql connectivity

--- StockSharer/Controllers/SearchController.cs
+++ StockSharer/Controllers/SearchController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+using Dapper;
+using MySql.Data.MySqlClient;
 
 namespace StockSharer.Controllers
 {
@@ -6,7 +10,21 @@ namespace StockSharer.Controllers
     {
         public ActionResult Index(string q)
         {
-            return View();
+            using (var connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["StockSharerDatabase"].ToString()))
+            {
+                var sql = "SELECT FirstNumber FROM TestTable";
+                var number = connection.Query<int>(sql).FirstOrDefault();
+                var searchResultsViewModel = new SearchResultsViewModel
+                    {
+                        Number = number
+                    };
+                return View(searchResultsViewModel);
+            }
         }
-	}
+    }
+
+    public class SearchResultsViewModel
+    {
+        public int Number { get; set; }
+    }
 }
