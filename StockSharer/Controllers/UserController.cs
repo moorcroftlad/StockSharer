ed8f72f6e76aedf36ebc6b55311132e49951f92b
Andrew Moorcroft <moorcroftlad@hotmail.com>
2015-12-05 11:57:48 +0000

    added authentication

--- StockSharer/Controllers/UserController.cs
+++ StockSharer/Controllers/UserController.cs
@@ -1,27 +1,42 @@
 ﻿using System.Web.Mvc;
+using System.Web.Security;
 
 namespace StockSharer.Controllers
 {
     public class UserController : Controller
     {
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl = "")
         {
             var loginViewModel = (TempData["LoginViewModel"] as LoginViewModel) ?? new LoginViewModel();
-            return View(loginViewModel);
+            return User.Identity.IsAuthenticated ? LogOut() : View(loginViewModel);
         }
 
         [HttpPost]
         public ActionResult Login(LoginViewModel loginViewModel)
         {
+            if (Membership.ValidateUser(loginViewModel.Email, loginViewModel.Password))
+            {
+                return RedirectToAction("Index", "Home");
+            }
             loginViewModel.LoginError = true;
             TempData["LoginViewModel"] = loginViewModel;
             return Login();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
         }
     }
 
     public class LoginViewModel
     {
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
         public bool LoginError { get; set; }
     }
 }
