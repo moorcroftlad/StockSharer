27665b90a972c2552737b621a86a940c53a19eb4
Andrew Moorcroft <moorcroftlad@hotmail.com>
2016-01-18 20:23:37 +0000

    added nuget and build tasks

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,17 @@
-task :default do
-	sh "echo Hello World"
+require 'albacore'
+
+task :default => [:restore, :compile_this]
+
+#https://github.com/Albacore/albacore/wiki/nugets_restore
+nugets_restore :restore do |p|
+  p.out       = 'packages'
+  p.nuget_gem_exe
+end
+
+#https://github.com/Albacore/albacore/wiki/build
+build :compile_this do |b|
+  b.file   = Paths.join 'StockSharer.Web', 'StockSharer.Web.csproj'
+  b.target = ['Clean', 'Rebuild']
+  b.prop 'Configuration', 'Release'
+  b.logging = 'detailed'
 end
