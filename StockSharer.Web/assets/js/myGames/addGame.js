6348c4bfd204966b8efc5af3467f71a0b7dbd3a7
Andrew <moorcroftlad@hotmail.com>
2016-02-02 08:03:33 +0000

    close live search box when click elsewhere on page

--- StockSharer.Web/assets/js/myGames/addGame.js
+++ StockSharer.Web/assets/js/myGames/addGame.js
@@ -1,3 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#game-search input[name="term"]').liveSearch({ url: '/settings/games/search?term=' });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest('#live-search-term').length && !$(event.target).is('#live-search-term') && !$(event.target).is('#game-search input[name="term"]')) {
+            if ($('#live-search-term').is(":visible")) {
+                $('#live-search-term').hide();
+            }
+        } else {
+            $('#live-search-term').empty();
+            $('#live-search-term').show();
+        }
+    });
 });
