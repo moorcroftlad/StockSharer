f7783c437090a4dcf73560fbb14a32372e1c3a83
Andrew <moorcroftlad@hotmail.com>
2016-03-30 21:59:18 +0100

    added availability calendar to availability modal

--- StockSharer.Web/assets/js/search/availability.js
+++ StockSharer.Web/assets/js/search/availability.js
@@ -3,4 +3,23 @@ $(document).ready(function() {
         event.preventDefault();
         $('#availabilityModal').modal('show');
     });
+
+    var updateDate = function(start, end) {
+        $('#dateRange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    };
+
+    $('#dateRange').daterangepicker({
+        ranges: {
+            'One night': [window.moment(), window.moment().add(1, 'day')],
+            'Two nights': [window.moment(), window.moment().add(2, 'days')],
+            'Three nights': [window.moment(), window.moment().add(3, 'days')],
+            'One week': [window.moment(), window.moment().add(7, 'days')]
+        },
+        locale: {
+            format: 'YYYY-MM-DD'
+        },
+        minDate: window.moment()
+    }, updateDate);
+    
+    updateDate(window.moment(), window.moment().add(1, 'day'));
 });
