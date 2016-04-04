4f4b0b1a0424f79c38aeaacc0a4b44fd66a46d2e
Andrew <moorcroftlad@hotmail.com>
2016-04-04 20:21:12 +0100

    added address section

---
+++ StockSharer.Web/Areas/Settings/Controllers/AddressController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using StockSharer.Web.Data;
+
+namespace StockSharer.Web.Areas.Settings.Controllers
+{
+    public class AddressController : BaseSettingsController
+    {
+        private readonly AddressRepository _addressRepository = new AddressRepository();
+
+        public ActionResult Index()
+        {
+            var addressViewModel = TempData["ViewModel"] as AddressViewModel ?? new AddressViewModel();
+            addressViewModel.Address = _addressRepository.RetrieveAddress(User.UserId) ?? new Address();
+            return View(addressViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Address address)
+        {
+            //TODO: retrieve lat / long of postcode and return error message if cannot be found
+            address.Latitude = 0;
+            address.Longitude = 0;
+            address.UserId = User.UserId;
+            _addressRepository.UpdateAddress(address);
+            TempData["ViewModel"] = new AddressViewModel {Message = "Address updated", Success = true};
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class AddressViewModel
+    {
+        public Address Address { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
---
+++ StockSharer.Web/Data/AddressRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace StockSharer.Web.Data
+{
+    public class AddressRepository
+    {
+        private readonly string _connectionString;
+
+        public AddressRepository()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["StockSharerDatabase"].ToString();
+        }
+
+        public AddressRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public Address RetrieveAddress(int userId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                const string sql = @"   SELECT UserId, Line1, Line2, Town, County, Postcode, Latitude, Longitude 
+                                        FROM Address 
+                                        WHERE UserId = @UserId";
+                return connection.Query<Address>(sql, new { UserId = userId }).FirstOrDefault();
+            }
+        }
+
+        public void UpdateAddress(Address address)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                const string sql = @"   merge Address as target
+                                        using (values (@UserId))
+                                            as source (UserId)
+                                            on target.UserId = source.UserId
+                                        when matched then
+                                            update
+                                            SET Line1 = @Line1, Line2 = @Line2, Town = @Town, County = @County, Postcode = @Postcode, Latitude = @Latitude, Longitude = @Longitude
+                                        when not matched then
+                                            insert (UserId, Line1, Line2, Town, County, Postcode, Latitude, Longitude)
+                                            values (@UserId, @Line1, @Line2, @Town, @County, @Postcode, @Latitude, @Longitude);";
+                connection.Execute(sql, address);
+            }
+        }
+    }
+
+    public class Address
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+        public int UserId { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
